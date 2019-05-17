67dd7d0a7b6d3ec057d820d06f65ba40e535d819
kurosawa4434 <kurosawa4434@gmail.com>
2019-05-18 08:54:26 +0900

    transition boundary blocks

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -18,12 +18,13 @@ requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
              * paper
              *
              *----------------------------------------------*/
-            let max_width = 350
+            const max_width = 350
             const os = 10
             const SIZE = (max_width - os*2)
-                             / Math.max(...[4, width, height])
-            max_width = Math.min(350, SIZE*width+os*2)
-            const paper = Raphael(dom, max_width, SIZE*height+os*2, 0, 0)
+                / Math.max(...[4, width, height])
+            const paper = Raphael(
+                dom,Math.min(max_width, SIZE*width+os*2),
+                SIZE*height+os*2, 0, 0)
 
             /*----------------------------------------------*
              *
@@ -48,32 +49,33 @@ requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
 
             /*----------------------------------------------*
              *
-             * dictionary of boundary blocks
-             *
-             *----------------------------------------------*/
-            const boundary_dic = {}
-            output.forEach(o=>{
-                const [r, c] = o
-                boundary_dic[r*100+c] = 1
-            })
-
-            /*----------------------------------------------*
-             *
              * draw grid
              *
              *----------------------------------------------*/
+            const cell_dic = {}
             for (let r = 0; r < height; r += 1) {
                 for (let c = 0; c < width; c += 1) {
                     const cell = paper.rect(SIZE*c+os, 
                         SIZE*r+os, SIZE, SIZE)
-                    if (result && boundary_dic[r*100+c]) {
-                        cell.attr(attr.grid.boundary_block)
-                    } else if (grid[r][c] === 'X') {
+                    if (grid[r][c] === 'X') {
                         cell.attr(attr.grid.block)
                     } else {
                         cell.attr(attr.grid.empty)
                     }
+                    cell_dic[r*100+c] = cell
                 }
+            }
+
+            /*----------------------------------------------*
+             *
+             * transition the color of boundary blocks
+             *
+             *----------------------------------------------*/
+            if (result) {
+                output.forEach(([r, c])=>{
+                    cell_dic[r*100+c].animate(
+                        attr.grid.boundary_block, 1000)
+                })
             }
         }
 
